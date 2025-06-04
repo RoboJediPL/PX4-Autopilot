ebcb7e6f091753bdeb8b0bec3e475fe7315618ac
KrishnakumarSanthanam <129371088+KrishnakumarSanthanam@users.noreply.github.com>
2025-06-04 17:37:22 +1000

    Add basic quadruped control module

---
+++ src/modules/quadruped_control/QuadrupedControl.cpp
@@ -0,0 +1,116 @@
+/****************************************************************************
+ *
+ *   Copyright (c) 2025 PX4 Development Team. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ * 3. Neither the name PX4 nor the names of its contributors may be
+ *    used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ ****************************************************************************/
+
+#include "QuadrupedControl.hpp"
+
+QuadrupedControl::QuadrupedControl() :
+        ModuleParams(nullptr),
+        ScheduledWorkItem(MODULE_NAME, px4::wq_configurations::rate_ctrl)
+{
+}
+
+bool QuadrupedControl::init()
+{
+        ScheduleOnInterval(10_ms);
+        return true;
+}
+
+void QuadrupedControl::updateParams()
+{
+        ModuleParams::updateParams();
+}
+
+void QuadrupedControl::Run()
+{
+        if (should_exit()) {
+                ScheduleClear();
+                exit_and_cleanup();
+                return;
+        }
+
+        if (_parameter_update_sub.updated()) {
+                parameter_update_s p{};
+                _parameter_update_sub.copy(&p);
+                updateParams();
+        }
+
+        quadruped_leg_command_s cmd{};
+
+        if (_leg_command_sub.update(&cmd)) {
+                quadruped_leg_status_s status{};
+                status.timestamp = hrt_absolute_time();
+                memcpy(status.joint_position, cmd.joint_position, sizeof(status.joint_position));
+                memcpy(status.joint_velocity, cmd.joint_velocity, sizeof(status.joint_velocity));
+                _leg_status_pub.publish(status);
+        }
+}
+
+int QuadrupedControl::task_spawn(int argc, char *argv[])
+{
+        QuadrupedControl *instance = new QuadrupedControl();
+
+        if (instance && instance->init()) {
+                _object.store(instance);
+                _task_id = task_id_is_work_queue; // task id for scheduled work item
+                return 0;
+        }
+
+        delete instance;
+        return -1;
+}
+
+int QuadrupedControl::custom_command(int argc, char *argv[])
+{
+        return print_usage("unknown command");
+}
+
+int QuadrupedControl::print_usage(const char *reason)
+{
+        if (reason) {
+                PX4_INFO("%s", reason);
+        }
+
+        PRINT_MODULE_DESCRIPTION(
+                R"DESCR(
+### Description
+Simple quadruped control module example. It republishes leg commands as status.
+)DESCR");
+
+        PRINT_MODULE_USAGE_NAME("quadruped_control", "controller");
+        return 0;
+}
+
+extern "C" __EXPORT int quadruped_control_main(int argc, char *argv[])
+{
+        return QuadrupedControl::main(argc, argv);
+}
